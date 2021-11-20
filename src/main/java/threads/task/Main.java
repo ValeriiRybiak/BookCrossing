73df844254ff623c7627bb254369f198e20fbab0
Valerii Rybiak <Valeriy.Rybiak@sunpowercorp.com>
2021-11-20 18:58:48 +0200

    Fixed and modified thread task

--- src/main/java/threads/task/Main.java
+++ src/main/java/threads/task/Main.java
@@ -2,12 +2,18 @@ package threads.task;
 
 import lombok.SneakyThrows;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
-import static common_utils.TaskHelper.getRandomString;
+import static java.lang.String.valueOf;
 import static java.lang.Thread.currentThread;
 import static java.lang.Thread.sleep;
-import static java.util.Collections.synchronizedList;
 
 /**
  * Сделать симуляцию общего чата между двумя потоками. Каждый поток должен передавать сообщения через общую очередь
@@ -26,55 +32,109 @@ import static java.util.Collections.synchronizedList;
  * разными способами (смотрим способы создания потоков)
  **/
 class Client implements Runnable {
-    private static volatile Random random = new Random();
-    public static volatile List<String> messages = synchronizedList(new ArrayList<>());
-    private static volatile String name = "";
+    private static final Random random = new Random();
+    public static Map<String, List<String>> messages = new ConcurrentHashMap<>();
+    private static int counter = 0;
 
-    public void setName(String name) {
-        Client.name = name;
-    }
-
-    private String getThreadName() {
+    private String getCurrentThreadName() {
         return currentThread().getName();
     }
 
-    public synchronized void writeRandomMessage() {
-        messages.add(getThreadName() + " " + getRandomString());
-        System.out.println("                                        write " + getThreadName());
+    public void writeRandomMessage() {
+        try {
+            messages.get(getCurrentThreadName()).add(valueOf(++counter));
+        } catch (NullPointerException e) {
+            messages.put(getCurrentThreadName(), new ArrayList<>());
+            messages.get(getCurrentThreadName()).add(valueOf(counter));
+        }
+        System.out.println("                                        write " + getCurrentThreadName() + " value: " + counter);
+    }
+
+    private Map<String, List<String>> getMessagesFromOtherThreads() {
+        Supplier<Map<String, List<String>>> messages = () -> Client.messages
+                .entrySet()
+                .stream()
+                .filter(map -> !map.getKey().equals(getCurrentThreadName()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        return messages.get();
     }
 
     public void readNewMessages() {
-        synchronized (messages) {
-            System.out.println(getThreadName() + " unread messages:" + messages.size());
-            messages.forEach(System.out::println);
-        }
+        System.out.println(getCurrentThreadName() + " unread messages: " + getMessagesFromOtherThreads().toString());
+        printMessagesNewMessagesFromOtherThreads();
+        clearHistory();
+    }
 
+    private void printMessagesNewMessagesFromOtherThreads() {
+        getMessagesFromOtherThreads()
+                .forEach((key, value) -> System.out.println("From: " + key + " message: " +
+                        value
+                                .stream()
+                                .collect(Collector.of(
+                                        StringBuilder::new,
+                                        (sb, message) -> sb.append(message).append(", "),
+                                        StringBuilder::append,
+                                        sb -> {
+                                            var messages = sb.toString();
+                                            if (messages.endsWith(", "))
+                                                return messages.substring(0, messages.length() - 2);
+                                            return messages;
+                                        }
+                                ))
+                ));
+    }
+
+    private void printMessagesOfInterruptedThreads() {
+        System.out.println("From: " + getCurrentThreadName() + " message: " + messages
+                .get(getCurrentThreadName())
+                .stream()
+                .collect(Collector.of(
+                        StringBuilder::new,
+                        (sb, message) -> sb.append(message).append(", "),
+                        StringBuilder::append,
+                        sb -> {
+                            var messages = sb.toString();
+                            if (messages.endsWith(", "))
+                                return messages.substring(0, messages.length() - 2);
+                            return messages;
+                        }
+                )) + " interrupt ______________________________"
+        );
     }
 
     public void isThreadActive() {
-        if (messages.size() >= 10) {
+        var isOneOfThreadWroteMoreThan3Messages = getMessagesFromOtherThreads()
+                .values()
+                .stream()
+                .anyMatch(x -> x.size() >= 3);
+        var isCurrentThreadWroteMoreThan3Messages = messages.get(getCurrentThreadName()).size() >= 3;
+        if (isOneOfThreadWroteMoreThan3Messages || isCurrentThreadWroteMoreThan3Messages) {
+            printMessagesOfInterruptedThreads();
+            clearOwnHistory();
             currentThread().interrupt();
-            System.out.println(getThreadName() + " interrupt()");
         }
     }
 
-    public synchronized int getRandomTimeout() {
-        return random.nextInt(2 - 1) + 1;
+    public int getRandomTimeout() {
+        return random.nextInt(2000) + 1;
     }
 
+    private void clearHistory() {
+        getMessagesFromOtherThreads().forEach((key, value) -> value.clear());
+    }
+
+    private void clearOwnHistory() {
+        messages.remove(getCurrentThreadName());
+    }
 
     @SneakyThrows
     @Override
     public void run() {
         while (!currentThread().isInterrupted()) {
-            synchronized (messages) {
-                if (!name.equals(currentThread().getName())) {
-                    readNewMessages();
-                    messages.clear();
-                }
-                setName(currentThread().getName());
+            sleep(getRandomTimeout());
+            synchronized (random) {
+                if (!messages.isEmpty()) readNewMessages();
                 writeRandomMessage();
-                Thread.sleep(getRandomTimeout());
                 isThreadActive();
             }
         }
@@ -86,18 +146,24 @@ public class Main {
     public static void main(String[] args) {
         var a = new Thread(new Client(), "a");
         var b = new Thread(new Client(), "b");
+        var c = new Thread(new Client(), "c");
         a.start();
-        Thread.sleep(5);
         b.start();
-        var c = false;
-        while (!c) {
-            if (a.isInterrupted() || b.isInterrupted()) {
-                a.join();
-                b.join();
+        c.start();
+        var isAllThreadsAreInterrupted = false;
+        while (!isAllThreadsAreInterrupted) {
+            synchronized (new Object()) {
+                if (!a.isAlive() && a.isInterrupted()) a.join();
+                if (!b.isAlive() && b.isInterrupted()) b.join();
+                if (!c.isAlive() && c.isInterrupted()) c.join();
+                if (!a.isAlive() && !b.isAlive() && !c.isAlive()) isAllThreadsAreInterrupted = true;
+                System.out.println("__________________MAIN__________________");
+                System.out.println("a State -> " + a.getState());
+                System.out.println("b State -> " + b.getState());
+                System.out.println("c State -> " + c.getState());
+                System.out.println("__________________MAIN__________________");
             }
-            if (!a.isAlive() && !b.isAlive()) c = true;
-            System.out.println("MAIN");
-            sleep(100);
+            sleep(1000);
         }
         System.out.println("END MAIN");
     }
